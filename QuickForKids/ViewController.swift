fec222122890c833a7b52dc576a8d45f29cb91ba
Poonam Pandey <mspoonam.contact@gmail.com>
2018-12-06 08:59:09 +0530

    Initial commit with iOS project structure

---
+++ QuickForKids/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickForKids
+//
+//  Created by Poonam Pandey on 06/12/18.
+//  Copyright © 2018 Poonam Pandey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
